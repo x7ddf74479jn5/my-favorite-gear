d667fc64f9ad5cb5f98a5a8aea08e60f6201cd9f
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-06-28 23:56:02 +0900

    wip: setup project

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
+    "@storybook/addon-storyshots",
+  ],
+};
