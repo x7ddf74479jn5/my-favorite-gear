0deda849fb4d0a7614ea5bafecc66d9a8c8e8abf
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-07-13 20:25:47 +0900

    ⭐️ add: Progress test

---
+++ test/components/common/progress/Progress.test.tsx
@@ -0,0 +1,24 @@
+import Progress from "components/common/progress/Progress";
+import React from "react";
+
+import { render } from "../../../test-utils";
+
+describe("Progress", () => {
+  it("matches snapshot", () => {
+    const renderResult = render(<Progress />);
+
+    expect(renderResult.asFragment()).toMatchSnapshot();
+  });
+
+  it("renders correctly", () => {
+    const renderResult = render(<Progress />);
+    expect(renderResult.container.firstChild).toHaveAttribute(
+      "class",
+      expect.stringContaining("styledContainer")
+    );
+
+    expect(renderResult.getByText("読み込み中")).toBeInTheDocument();
+    expect(renderResult.container.getElementsByTagName("svg")).toBeTruthy();
+    expect(renderResult.container.getElementsByTagName("circle")).toBeTruthy();
+  });
+});
