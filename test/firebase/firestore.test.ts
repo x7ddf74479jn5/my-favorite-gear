ff9571616bf34c968ddc13478a1da1d99de58b18
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-07-28 21:25:10 +0900

    ⛱ update: firebase rules

--- test/firebase/firestore.test.ts
+++ test/firebase/firestore.test.ts
@@ -6,6 +6,7 @@ import * as firebase from "@firebase/rules-unit-testing";
 import * as admin from "firebase-admin";
 import * as fs from "fs";
 import * as path from "path";
+import { collectionName } from "services/constants";
 import type { User } from "services/models/user";
 
 const PROJECT_ID = "dev-my-favorite-gear";
@@ -40,7 +41,7 @@ afterAll(async () => {
   );
 });
 
-jest.setTimeout(10000);
+jest.setTimeout(15000);
 
 const correctUserData: User = {
   screenName: "screenName",
@@ -64,202 +65,218 @@ const adminData: User = {
   updatedAt: admin.firestore.Timestamp.fromDate(new Date()),
 };
 
-describe("ユーザー認証情報の検証", () => {
-  test("自分のuidと同様のドキュメントIDのユーザー情報だけを閲覧、作成、編集可能", async () => {
-    const db = createAuthApp({ uid: "uid" });
-    const userDocumentRef = db.collection("users").doc("uid");
-    await firebase.assertSucceeds(userDocumentRef.set(correctUserData));
-    await firebase.assertSucceeds(userDocumentRef.get());
-    await firebase.assertSucceeds(
-      userDocumentRef.update({ screenName: "newUid" })
-    );
+describe("/users/{userId}", () => {
+  describe("ユーザー認証情報の検証", () => {
+    test("自分のuidと同様のドキュメントIDのユーザー情報だけを閲覧、作成、編集可能", async () => {
+      const db = createAuthApp({ uid: "uid" });
+      const userDocumentRef = db.collection(collectionName.users).doc("uid");
+      await firebase.assertSucceeds(userDocumentRef.set(correctUserData));
+      await firebase.assertSucceeds(userDocumentRef.get());
+      await firebase.assertSucceeds(
+        userDocumentRef.update({ screenName: "newUid" })
+      );
+    });
+
+    test("自分のuidと異なるドキュメントは閲覧、作成、編集が出来ない", async () => {
+      createAdminApp()
+        .collection(collectionName.users)
+        .doc("uid_1")
+        .set(adminData);
+      const db = createAuthApp({ uid: "uid_2" });
+      const userDocumentRef = db.collection(collectionName.users).doc("uid_1");
+      await firebase.assertFails(userDocumentRef.set(correctUserData));
+      await firebase.assertFails(userDocumentRef.get());
+      await firebase.assertFails(
+        userDocumentRef.update({ screenName: "uid_2" })
+      );
+    });
+
+    test("screenNameが合致したドキュメントは取得できる", async () => {
+      createAdminApp()
+        .collection(collectionName.users)
+        .doc("uid_1")
+        .set(adminData);
+      const db = createAuthApp({ uid: "uid_2" });
+      const matchedUserDocuments = db
+        .collection(collectionName.users)
+        .where("screenName", "==", "screenName");
+      await firebase.assertSucceeds(matchedUserDocuments.get());
+    });
   });
 
-  test("自分のuidと異なるドキュメントは閲覧、作成、編集が出来ない", async () => {
-    createAdminApp().collection("users").doc("uid_1").set(adminData);
-    const db = createAuthApp({ uid: "uid_2" });
-    const userDocumentRef = db.collection("users").doc("uid_1");
-    await firebase.assertFails(userDocumentRef.set(correctUserData));
-    await firebase.assertFails(userDocumentRef.get());
-    await firebase.assertFails(userDocumentRef.update({ screenName: "uid_2" }));
-  });
+  describe("スキーマの検証", () => {
+    test("正しくないスキーマの場合は作成できない", async () => {
+      const db = createAuthApp({ uid: "uid" });
+      const userDocumentRef = db.collection(collectionName.users).doc("uid");
 
-  test("screenNameが合致したドキュメントは取得できる", async () => {
-    createAdminApp().collection("users").doc("uid_1").set(adminData);
-    const db = createAuthApp({ uid: "uid_2" });
-    const matchedUserDocuments = db
-      .collection("users")
-      .where("screenName", "==", "screenName");
-    await firebase.assertSucceeds(matchedUserDocuments.get());
-  });
-});
+      // 想定外のプロパティがある場合
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, place: "japan" })
+      );
 
-describe("スキーマの検証", () => {
-  test("正しくないスキーマの場合は作成できない", async () => {
-    const db = createAuthApp({ uid: "uid" });
-    const userDocumentRef = db.collection("users").doc("uid");
-
-    // 想定外のプロパティがある場合
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, place: "japan" })
-    );
-
-    const insufficientData = {
-      displayName: "displayName",
-      description: "description",
-      photoUrl: "https://photoUrl.com",
-      provider: "twitter",
-      providerUid: "providerUid",
-      createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
-      updatedAt: firebase.firestore.Timestamp.fromDate(new Date()),
-    };
-
-    // プロパティが不足している場合
-    await firebase.assertFails(userDocumentRef.set(insufficientData));
-
-    // プロパティの型が異なる場合
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, screenName: 1234 })
-    );
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, displayName: 1234 })
-    );
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, description: 1234 })
-    );
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, provider: 1234 })
-    );
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, photoUrl: 1234 })
-    );
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, provider: 1234 })
-    );
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, providerUid: 1234 })
-    );
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, createdAt: "1" })
-    );
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, updatedAt: "1" })
-    );
-  });
-
-  test("正しくないスキーマの場合は編集できない", async () => {
-    createAdminApp().collection("users").doc("uid").set(adminData);
-    const db = createAuthApp({ uid: "uid" });
-    const userDocumentRef = db.collection("users").doc("uid");
-
-    // 想定外のプロパティがある場合
-    await firebase.assertFails(userDocumentRef.update({ place: "japan" }));
-
-    // プロパティの型が異なる場合
-    await firebase.assertFails(userDocumentRef.update({ screenName: 1234 }));
-    await firebase.assertFails(userDocumentRef.update({ displayName: 1234 }));
-    await firebase.assertFails(userDocumentRef.update({ description: 1234 }));
-    await firebase.assertFails(userDocumentRef.update({ provider: 1234 }));
-    await firebase.assertFails(userDocumentRef.update({ photoUrl: 1234 }));
-    await firebase.assertFails(userDocumentRef.update({ provider: 1234 }));
-    await firebase.assertFails(userDocumentRef.update({ providerUid: 1234 }));
-    await firebase.assertFails(userDocumentRef.update({ createdAt: "1" }));
-    await firebase.assertFails(userDocumentRef.update({ updatedAt: "1" }));
-  });
-});
-
-describe("値のバリデーション", () => {
-  test("screenNameは1文字以上である", async () => {
-    const db = createAuthApp({ uid: "uid" });
-    const userDocumentRef = db.collection("users").doc("uid");
-
-    await firebase.assertSucceeds(
-      userDocumentRef.set({
-        ...correctUserData,
-        screenName: "screenName",
-      })
-    );
-
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, screenName: "" })
-    );
-  });
-
-  test("displayは1文字以上である", async () => {
-    const db = createAuthApp({ uid: "uid" });
-    const userDocumentRef = db.collection("users").doc("uid");
-
-    await firebase.assertSucceeds(
-      userDocumentRef.set({
-        ...correctUserData,
+      const insufficientData = {
         displayName: "displayName",
-      })
-    );
-
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, displayName: "" })
-    );
-  });
-
-  test("photoUrlはURL(https)である", async () => {
-    const db = createAuthApp({ uid: "uid" });
-    const userDocumentRef = db.collection("users").doc("uid");
-
-    await firebase.assertSucceeds(
-      userDocumentRef.set({
-        ...correctUserData,
-        photoUrl: "https://example.com",
-      })
-    );
-
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, photoUrl: "" })
-    );
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, photUrl: "http://exmaple.com" })
-    );
-  });
-
-  test("providerはtwitterである", async () => {
-    const db = createAuthApp({ uid: "uid" });
-    const userDocumentRef = db.collection("users").doc("uid");
-
-    await firebase.assertSucceeds(
-      userDocumentRef.set({
-        ...correctUserData,
+        description: "description",
+        photoUrl: "https://photoUrl.com",
         provider: "twitter",
-      })
-    );
+        providerUid: "providerUid",
+        createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+        updatedAt: firebase.firestore.Timestamp.fromDate(new Date()),
+      };
 
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, provider: "" })
-    );
-    await firebase.assertFails(
-      userDocumentRef.set({
-        ...correctUserData,
-        provider: "github",
-      })
-    );
+      // プロパティが不足している場合
+      await firebase.assertFails(userDocumentRef.set(insufficientData));
+
+      // プロパティの型が異なる場合
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, screenName: 1234 })
+      );
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, displayName: 1234 })
+      );
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, description: 1234 })
+      );
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, provider: 1234 })
+      );
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, photoUrl: 1234 })
+      );
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, provider: 1234 })
+      );
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, providerUid: 1234 })
+      );
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, createdAt: "1" })
+      );
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, updatedAt: "1" })
+      );
+    });
+
+    test("正しくないスキーマの場合は編集できない", async () => {
+      createAdminApp()
+        .collection(collectionName.users)
+        .doc("uid")
+        .set(adminData);
+      const db = createAuthApp({ uid: "uid" });
+      const userDocumentRef = db.collection(collectionName.users).doc("uid");
+
+      // 想定外のプロパティがある場合
+      await firebase.assertFails(userDocumentRef.update({ place: "japan" }));
+
+      // プロパティの型が異なる場合
+      await firebase.assertFails(userDocumentRef.update({ screenName: 1234 }));
+      await firebase.assertFails(userDocumentRef.update({ displayName: 1234 }));
+      await firebase.assertFails(userDocumentRef.update({ description: 1234 }));
+      await firebase.assertFails(userDocumentRef.update({ provider: 1234 }));
+      await firebase.assertFails(userDocumentRef.update({ photoUrl: 1234 }));
+      await firebase.assertFails(userDocumentRef.update({ provider: 1234 }));
+      await firebase.assertFails(userDocumentRef.update({ providerUid: 1234 }));
+      await firebase.assertFails(userDocumentRef.update({ createdAt: "1" }));
+      await firebase.assertFails(userDocumentRef.update({ updatedAt: "1" }));
+    });
   });
 
-  test("providerUidは1文字以上である", async () => {
-    const db = createAuthApp({ uid: "uid" });
-    const userDocumentRef = db.collection("users").doc("uid");
+  describe("値のバリデーション", () => {
+    test("screenNameは1文字以上である", async () => {
+      const db = createAuthApp({ uid: "uid" });
+      const userDocumentRef = db.collection(collectionName.users).doc("uid");
 
-    await firebase.assertSucceeds(
-      userDocumentRef.set({
-        ...correctUserData,
-        providerUid: "providerUid",
-      })
-    );
+      await firebase.assertSucceeds(
+        userDocumentRef.set({
+          ...correctUserData,
+          screenName: "screenName",
+        })
+      );
 
-    await firebase.assertFails(
-      userDocumentRef.set({ ...correctUserData, providerUid: "" })
-    );
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, screenName: "" })
+      );
+    });
+
+    test("displayは1文字以上である", async () => {
+      const db = createAuthApp({ uid: "uid" });
+      const userDocumentRef = db.collection(collectionName.users).doc("uid");
+
+      await firebase.assertSucceeds(
+        userDocumentRef.set({
+          ...correctUserData,
+          displayName: "displayName",
+        })
+      );
+
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, displayName: "" })
+      );
+    });
+
+    test("photoUrlはURL(https)である", async () => {
+      const db = createAuthApp({ uid: "uid" });
+      const userDocumentRef = db.collection(collectionName.users).doc("uid");
+
+      await firebase.assertSucceeds(
+        userDocumentRef.set({
+          ...correctUserData,
+          photoUrl: "https://example.com",
+        })
+      );
+
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, photoUrl: "" })
+      );
+      await firebase.assertFails(
+        userDocumentRef.set({
+          ...correctUserData,
+          photUrl: "http://exmaple.com",
+        })
+      );
+    });
+
+    test("providerはtwitterである", async () => {
+      const db = createAuthApp({ uid: "uid" });
+      const userDocumentRef = db.collection(collectionName.users).doc("uid");
+
+      await firebase.assertSucceeds(
+        userDocumentRef.set({
+          ...correctUserData,
+          provider: "twitter",
+        })
+      );
+
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, provider: "" })
+      );
+      await firebase.assertFails(
+        userDocumentRef.set({
+          ...correctUserData,
+          provider: "github",
+        })
+      );
+    });
+
+    test("providerUidは1文字以上である", async () => {
+      const db = createAuthApp({ uid: "uid" });
+      const userDocumentRef = db.collection(collectionName.users).doc("uid");
+
+      await firebase.assertSucceeds(
+        userDocumentRef.set({
+          ...correctUserData,
+          providerUid: "providerUid",
+        })
+      );
+
+      await firebase.assertFails(
+        userDocumentRef.set({ ...correctUserData, providerUid: "" })
+      );
+    });
   });
 });
 
-describe("カウンターの検証", () => {
+describe("/docCounters/users", () => {
   const correctCounterData = {
     count: 1,
     updatedAt: firebase.firestore.Timestamp.fromDate(new Date()),
@@ -267,14 +284,18 @@ describe("カウンターの検証", () => {
 
   test("ユーザーが認証済みの場合は書き込みできる", async () => {
     const db = createAuthApp({ uid: "uid" });
-    const usersDocumentRef = db.collection("docCounters").doc("users");
+    const usersDocumentRef = db
+      .collection(collectionName.docCounters)
+      .doc("users");
 
     await firebase.assertSucceeds(usersDocumentRef.set(correctCounterData));
   });
 
   test("正しくないスキーマの場合は書き込みできない", async () => {
     const db = createAuthApp({ uid: "uid" });
-    const usersDocumentRef = db.collection("docCounters").doc("users");
+    const usersDocumentRef = db
+      .collection(collectionName.docCounters)
+      .doc("users");
 
     await firebase.assertFails(
       usersDocumentRef.set({
@@ -299,5 +320,55 @@ describe("カウンターの検証", () => {
         counter: 1,
       })
     );
+  });
+});
+
+describe("/playlists/{playlistId}", () => {
+  const correctPlaylistData = {
+    id: "uid",
+    twitterId: "twitterId",
+    image: "image_normal",
+    songs: [],
+    songsCount: 8,
+    updatedAt: firebase.firestore.Timestamp.fromDate(new Date()),
+  };
+  const adminPlaylistData = {
+    id: "uid",
+    twitterId: "twitterId",
+    image: "image_normal",
+    songs: [],
+    songsCount: 8,
+    updatedAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+  describe("ユーザー認証情報の検証", () => {
+    test("自分のuidと同様のドキュメントIDのユーザー情報だけを閲覧、作成、編集可能", async () => {
+      const db = createAuthApp({ uid: "uid" });
+      const playlistDocumentRef = db
+        .collection(collectionName.playlists)
+        .doc("uid");
+      await firebase.assertSucceeds(
+        playlistDocumentRef.set(correctPlaylistData)
+      );
+      await firebase.assertSucceeds(playlistDocumentRef.get());
+      await firebase.assertSucceeds(
+        playlistDocumentRef.update({ image: "newImage" })
+      );
+    });
+
+    test("自分のuidと異なるドキュメントは閲覧、作成、編集が出来ない", async () => {
+      createAdminApp()
+        .collection(collectionName.playlists)
+        .doc("uid_1")
+        .set(adminPlaylistData);
+      const db = createAuthApp({ uid: "uid_2" });
+      const playlistDocumentRef = db
+        .collection(collectionName.users)
+        .doc("uid_1");
+      await firebase.assertFails(playlistDocumentRef.set(correctPlaylistData));
+      await firebase.assertFails(playlistDocumentRef.get());
+      await firebase.assertFails(
+        playlistDocumentRef.update({ image: "newImage" })
+      );
+    });
   });
 });
