007d9d91b45f647b93190f02eaa86807795c4461
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-11-30 17:23:14 +0900

    🍬 chore

--- src/containers/FavoriteList/FavoriteList.tsx
+++ src/containers/FavoriteList/FavoriteList.tsx
@@ -7,11 +7,9 @@ import TweetButton from "components/common/tweet/TweetButton";
 import useFavoriteList from "hooks/use-favoriteList";
 import paths from "paths";
 import type { FC } from "react";
-import React, { useState } from "react";
+import React from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
-import type { Gear } from "services/models/gear";
-import { blankGear } from "services/models/gear";
 import type { User } from "services/models/user";
 
 const FavoriteListContainer: FC<{ user: User | null }> = ({ user }) => {
@@ -19,28 +17,7 @@ const FavoriteListContainer: FC<{ user: User | null }> = ({ user }) => {
   const favoriteList = useFavoriteList({
     id: id,
   });
-  const [playMusicsGear, setPlayMusicsGear] = useState<Gear>(blankGear);
-  const playMusics = () => {
-    const musics: HTMLAudioElement[] = Array.prototype.slice.call(
-      document.getElementsByTagName("audio")
-    );
-    musics.forEach((music) => {
-      music.pause();
-    });
-    const playMusic = (n: number) => {
-      setPlayMusicsGear(favoriteList.favoriteList.gears[n]);
-      musics[n].currentTime = 0;
-      musics[n].play();
-      const next = () => {
-        musics[n].removeEventListener("ended", next);
-        n = n + 1;
-        if (n >= musics.length) n = 0;
-        playMusic(n);
-      };
-      musics[n].addEventListener("ended", next);
-    };
-    playMusic(0);
-  };
+
   if (favoriteList.loading) return <Progress />;
   if (favoriteList.favoriteList.gears.length < 8)
     return (
@@ -70,29 +47,8 @@ const FavoriteListContainer: FC<{ user: User | null }> = ({ user }) => {
       <Typography variant="h6" gutterBottom align="center">
         {favoriteList.favoriteList.twitterId}のMy Favorite gear
       </Typography>
-      {playMusicsGear.mediumImageUrl ? (
-        <MakeImage gear={playMusicsGear} />
-      ) : (
-        <MakeImage favoriteList={favoriteList.favoriteList} />
-      )}
-      <Typography align="center">{playMusicsGear.productName}</Typography>
-      <Typography align="center">{playMusicsGear.makerName}</Typography>
-
-      {playMusicsGear.productName === "" && (
-        <Button
-          variant="contained"
-          color="secondary"
-          fullWidth
-          onClick={() => {
-            return playMusics();
-          }}
-        >
-          連続再生
-        </Button>
-      )}
-
+      <MakeImage favoriteList={favoriteList.favoriteList} />
       <TweetButton favoriteList={favoriteList.favoriteList} />
-
       <GearCards gears={favoriteList.favoriteList.gears} />
       <Button
         variant="contained"
