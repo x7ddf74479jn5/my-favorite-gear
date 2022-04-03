ad86c747a396f34bed5ac006be5781369f1a2cf8
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-04-04 01:16:38 +0900

    ⚡️ improve: code splitting in router

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,16 +1,16 @@
 import type { VFC } from "react";
-import React, { useContext } from "react";
+import React, { lazy, useContext } from "react";
 import { Route, Routes } from "react-router";
 
-import FavoriteList from "@/components/FavoriteList";
-import FavoriteLists from "@/components/FavoriteLists";
-import MakeFavoriteList from "@/components/MakeFavoriteList";
-import Signin from "@/components/Signin";
 import { UserContext } from "@/contexts";
 import paths from "@/paths";
 
 export const AppRouter: VFC = () => {
   const { user } = useContext(UserContext);
+  const FavoriteList = lazy(() => import("@/components/FavoriteList"));
+  const FavoriteLists = lazy(() => import("@/components/FavoriteLists"));
+  const MakeFavoriteList = lazy(() => import("@/components/MakeFavoriteList"));
+  const Signin = lazy(() => import("@/components/Signin"));
 
   return (
     <Routes>
